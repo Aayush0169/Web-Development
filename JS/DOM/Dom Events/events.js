9f9f0e4cf84bdf5b8876def80f79a29d832932f0
Ayush Kumar <rajaayush124212@gmail.com>
2024-11-16 13:00:11 +0530

    initial commit

--- JS/DOM/Dom Events/events.js
+++ JS/DOM/Dom Events/events.js
@@ -1,3 +1,5 @@
+//        Mouse Events
+//funtion for generating a color when button is clicked
 let btn=document.querySelector("button");
 function getrandomcolor(){
     let red=Math.floor(Math.random()*255);
@@ -14,3 +16,16 @@ btn.addEventListener("click",function(){
     h4.innerText=randomcolor;
     div.style.backgroundColor=randomcolor;
 })
+
+//using "this" for event handeling
+//adding single function on multiple elements
+
+let h2 =document.querySelector("h2");
+let h5=document.querySelector("h5");
+let h4=document.querySelector("h4");
+let backgroundcolor=function(){
+    this.style.backgroundColor="#8CB9BD";
+}
+h2.addEventListener("dblclick",backgroundcolor);
+h5.addEventListener("dblclick",backgroundcolor);
+h4.addEventListener("dblclick",backgroundcolor);
