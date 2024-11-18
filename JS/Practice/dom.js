e6e77c0cb92b6d1323dbe4cf03abf6a4451839ef
Ayush Kumar <rajaayush124212@gmail.com>
2024-11-18 18:08:37 +0530

    initial commit

--- JS/Practice/dom.js
+++ JS/Practice/dom.js
@@ -58,3 +58,23 @@ body.insertAdjacentElement('beforebegin',heading);
 let para2=document.createElement('p');
 para2.innerHTML="<p>Learning <b>Web Devlopment</b></p>"
 body.insertAdjacentElement('beforebegin',para2);
+
+// Qs6.Create a button on the page using JavaScript. Add an event listener to the button
+// that changes the button’s color to green when it is clicked.
+let bttn=document.querySelector("#bttn");
+bttn.addEventListener("click",()=>{
+    bttn.style.backgroundColor="green";
+})
+
+// Qs7. Create an input element on the page with a placeholder ”enter your name” and an
+// H2 heading on the page inside HTML.
+// The purpose of this input element is to enter a user’s name so it should only input
+// letters from a-z, A-Z and space (all other characters should not be detected).
+// Whenever the user inputs their name, their input should be dynamically visible inside
+// the heading.
+// [Please note that no other character apart from the allowed characters should be
+// visible in the heading]
+document.getElementById('nameinput').addEventListener('input', function() {
+    const filteredInput = this.value.replace(/[^a-zA-Z ]/g, ''); // Only allow letters and spaces
+    document.getElementById('nameHeading').textContent = filteredInput;
+});
