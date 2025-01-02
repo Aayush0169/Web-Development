f1eb04ae14efb9842b14b8977c874e99054803d7
Ayush Kumar <rajaayush124212@gmail.com>
2025-01-02 13:38:51 +0530

    initial commit

---
+++ Using Node/Express/Animal Page/index.js
@@ -0,0 +1,36 @@
+const express=require("express");
+const app=express();
+const path= require("path");
+const port=8080;
+
+//confirmation of server
+app.listen(port,()=>{
+    console.log(`server is listening on: ${port}`);
+})
+
+// setting views
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+
+//for home
+app.get('/',(req,res)=>{
+         res.render("home.ejs");
+})
+
+//with params
+app.get('/:username',(req,res)=>{
+    let{username}=req.params;
+    const instdata=require("./data.json");
+    const data= instdata[username];
+    console.log(data);
+    res.render("main.ejs",{data});
+})
+
+
+
+
+//for invalid input
+app.get('*',(req,res)=>{
+    console.log(req);
+    res.send("<h1>invalid URL</h1>");
+})
---
+++ Using Node/Express/Animal Page/readme.md
@@ -0,0 +1,2 @@
+Transfering data from 'json file' to web page as per the request/input. 
+Search /cats, /dogs, /lions to get result
