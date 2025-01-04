a4e07b717ef859b67a069bf4bea3bbf493c9dd23
Ayush Kumar <rajaayush124212@gmail.com>
2025-01-04 10:06:33 +0530

    initial commit

--- Using Node/Express/Animal Page/index.js
+++ Using Node/Express/Animal Page/index.js
@@ -8,9 +8,12 @@ app.listen(port,()=>{
     console.log(`server is listening on: ${port}`);
 })
 
-// setting views
+// setting views and static folder
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
+app.use(express.static(path.join(__dirname,"Public/style")));
+app.use(express.static(path.join(__dirname,"Public/js")));
+
 
 //for home
 app.get('/',(req,res)=>{
