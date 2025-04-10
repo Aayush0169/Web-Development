f2786ac2cbbf1327cc882cf9dea810af15eb1907
Ayush Kumar <rajaayush124212@gmail.com>
2025-04-10 16:56:17 +0530

    initial commit

---
+++ Express-Route/routes/post.js
@@ -0,0 +1,18 @@
+const express=require("express");
+const router=express.Router();
+
+//post route
+//index route
+router.get("/",(req,res)=>{
+    res.send("Post get  received! ");
+})
+//show post
+router.get("/:id",(req,res)=>{
+    res.send("get post id  post");
+})
+//update post
+router.put("/:id",(req,res)=>{
+    res.send("edit this post");
+})
+
+module.exports=router;
---
+++ Express-Route/routes/user.js
@@ -0,0 +1,20 @@
+const express=require("express");
+const router=express.Router();
+
+
+//user route
+//index
+router.get("/",(req,res)=>{
+    res.send("Get user request");
+})
+
+//user show
+router.get("/:id",(req,res)=>{
+    res.send("Hii , ssup");
+})
+//update user
+router.put("/:id",(req,res)=>{
+    res.send("updated");
+})
+
+module.exports=router;
