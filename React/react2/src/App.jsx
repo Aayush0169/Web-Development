b857f846a18f88ff7e2b3b18706b34cbeee4823b
Ayush Kumar <rajaayush124212@gmail.com>
2025-08-08 15:54:56 +0530

    initial commit

--- React/react2/src/App.jsx
+++ React/react2/src/App.jsx
@@ -1,16 +1,19 @@
 
 import './App.css'
 import Counter from './counter'
+import { sumOfArr } from './helper'
 import LikeBtn from './likeBtn'
 import Lottery from './lottery'
-
-
+import Ticket from './Ticket'
 
 
 function App() {
+  let winCondition=(ticket)=>{
+    return sumOfArr(ticket)===15;
+  }
   return (
     <div>
-      <Lottery n={4} winningSum={16}/>
+      <Lottery n={4}winCondition={winCondition}/>
     </div>
   )
 }
